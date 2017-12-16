9571c4f958351a938bb2e4a9a10059c57ea6c573
s.maslow <stasmaslov98@gmail.com>
2017-12-16 18:33:14 +0400

    initial commit

---
+++ src/classes/Solver.java
@@ -0,0 +1,8 @@
+package classes;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Solver {
+}
